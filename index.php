6a80d938e36892d902c31ac180caf31074a02c7e
Angelinakroptgkj <dreamer.reproduces.85@gmail.com>
2024-03-15 22:05:50 +0600

    bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "test";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./assets/css/reset.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+
+<body>
+
+
+  <script src="./assets/js/bootstrap.min.js"></script>
+  <script src="./assets/js/app.js"></script>
+</body>
+
+</html>
